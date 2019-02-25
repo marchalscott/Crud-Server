4b3c5ebbc3752f51ecc19d68a6b698b1113a5b0b
Marchal <marchalscott@gmail.com>
2019-02-25 10:37:16 -0700

    update store name

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product.model');
 
 exports.test = function (req, res) {
-    res.send('Welcome to  Marchals Store);
+    res.send('Welcome to  Marchals Store');
 };
 
 exports.all = (req, res) => {
