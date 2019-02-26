9559a3a05658d259be779fe11d1ccaee65270cf4
Marchal <marchalscott@gmail.com>
2019-02-25 21:31:02 -0700

    update products

--- models/product.model.js
+++ models/product.model.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 let ProductSchema = new Schema({
     name: { type: String, required: true, max: 100 },
     price: { type: Number, required: true},
-    size: { type: String, required: false },
     color: { type: String, required: false },
+    size: { type: String, required: false },
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
